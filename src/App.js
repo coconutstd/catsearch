54dcf8ce645bbd89986e9dfa92bac57823071920
junui <wnsspdl@naver.com>
2021-05-14 20:32:54 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+console.log("app is running!");
+
+import { api } from './api/api.js';
+
+class App {
+    $target = null;
+    data = [];
+
+    constructor($target) {
+        this.$target = $target;
+
+        this.searchInput = new SearchInput({
+            $target,
+            onSearch: keyword => {
+                api.fetchCats(keyword).then(({ data }) => this.setState(data));
+            }
+        });
+
+        this.searchResult = new SearchResult({
+            $target,
+            initialData: this.data,
+            onClick: image => {
+                this.imageInfo.setState({
+                    visible: true,
+                    image
+                });
+            }
+        });
+
+        this.imageInfo = new ImageInfo({
+            $target,
+            data: {
+                visible: false,
+                image: null
+            }
+        });
+    }
+
+    setState(nextData) {
+        console.log(this);
+        this.data = nextData;
+        this.searchResult.setState(nextData);
+    }
+}
